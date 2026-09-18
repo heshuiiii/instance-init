beceddc77a9fc17ef6e992702730c44f9cea1a84
heshuiiii <71701401+heshuiiii@users.noreply.github.com>
2026-09-18 19:46:36 +0800

    Update mult_qb_enhanced.sh

--- qbittorrent_mult/mult_qb_enhanced.sh
+++ qbittorrent_mult/mult_qb_enhanced.sh
@@ -2,6 +2,6 @@ echo "⚠️ 此脚本已废弃，正在跳转到新版本..."
 
 sleep 2
 
-bash <(curl -sL https://raw.githubusercontent.com/vioeloria/PT/refs/heads/main/PT脚本/multqbittorrent.sh)
+bash <(curl -sL https://raw.githubusercontent.com/vioeloria/vertex_integrate/refs/heads/main/multqbittorrent.sh)
 
 exit 0
